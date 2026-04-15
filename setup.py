d34517abb6247129f626c25f181410674aa6cb98
shruti chauhan <shrutichauhan792004@gmail.com>
2026-04-15 19:37:05 +0530

    data ingestion completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shruti Chauhan",
+    author_email="shrutichauhan792004@gmail.com",
+    packages=find_packages()
+)
